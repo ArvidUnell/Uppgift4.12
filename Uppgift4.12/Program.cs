42a8499e670e6129b79beea42dbc07f76a2e4630
arun06001 <arun06001@E-PW0234KE>
2023-10-20 11:00:30 +0200

    Add project files.

---
+++ Uppgift4.12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Upgift4_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bestäm kvadraternas sidlängd");
+            byte sidlängd = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Bestäm rutnätets bredd");
+            byte bredd = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Bestäm rutnätets höjd");
+            byte höjd = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 0; i < höjd; i++)
+            {
+                for(int j = 0; j < sidlängd; j++)
+                {
+                    for (int k = 0; k < bredd; k++)
+                    {
+                        for (int l = 0; l < sidlängd; l++)
+                        {
+                            Console.Write('*');
+                        }
+                        Console.Write(' ');
+                    }
+                    Console.WriteLine();
+                }
+            Console.WriteLine();
+            }
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
